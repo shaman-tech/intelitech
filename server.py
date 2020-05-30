f377f8507afd0e442451b4e455289668f0ddcb21
Julian Cowell <julian43w@outlook.com>
2020-05-30 09:20:53 -0500

    create_meeting endpoint now works

--- server.py
+++ server.py
@@ -30,15 +30,15 @@ client = ZoomClient(INTELITECH_API_KEY, INTELITECH_API_SECRET)
 
 @app.route('/create_meeting', methods=['POST'])
 def create_meeting():
-    req_data = request.get_json()
+    req_data = request.get_json()['data']
     date_array = req_data["appt_date"].split('-')
     time_array = req_data["appt_time"].split(':')
-    mins = time_array[1].split[" "]
+    mins = time_array[1].split(" ")
     time_array[1] = mins[0]
     if mins[1] == 'PM':
-        time_array[0] += 12
-    date_time = datetime(2020, date_array[0],date_array[1],time_array[0],time_array[1],0)
-    client.meeting.create(
+        time_array[0] = int(time_array[0]) + 12
+    date_time = datetime(2020, int(date_array[0]),int(date_array[1]),int(time_array[0]),int(time_array[1]),0)
+    meeting = client.meeting.create(
         user_id=INTELITECH_HOST_ID,
         meeting_type=2,
         start_time= date_time,
@@ -46,23 +46,29 @@ def create_meeting():
         timezone='America/Chicago',
         password='pawpeq',
         topic=f'Consultation with {req_data["name"]}, age {req_data["age"]}',
-        agenda=''
+        settings={
+            "auto_recording":True,
+            "host_video":True,
+            "participant_video":True,
+        },
+        
+        agenda=f'{req_data["agenda"]}'
     )
 
-    user_list_response = client.user.list()
-    user_list = json.loads(user_list_response.content)
+    # user_list_response = client.user.list()
+    # user_list = json.loads(user_list_response.content)
 
-    ret_data = {}
-    for user in user_list['users']:
-        user_id = user['id']
-        meeting = json.loads(client.meeting.list(user_id=user_id).content)
-        
-        topic = meeting["meetings"][0]['topic'])
-        if topic.find(req_data["name"]) != -1:
-            ret_data["join_url"] = meeting["meetings"][0]['join_url']
-            ret_data["start_time"]=meeting["meetings"][0]['start_time']
-
-    return ret_data
+    # ret_data = {}
+    # for user in user_list['users']:
+    #     user_id = user['id']
+    #     meeting = json.loads(client.meeting.list(user_id=user_id).content)
+    #     ret_data[user_id] = meeting
+        # topic = meeting["meetings"][0]['topic']
+        # if topic.find(req_data["name"]) != -1:
+        #     ret_data["join_url"] = meeting["meetings"][0]['join_url']
+        #     ret_data["start_time"]=meeting["meetings"][0]['start_time']
+        #     return ret_data
+    return meeting.content
 
     
 if __name__ == '__main__':
